552fbd564b690485015a6fc02081bca94de1e59f
Kerim Demir <kerim.demir2002@hotmail.com>
2023-05-26 17:29:09 +0300

    fix

--- heuristic_imp.py
+++ heuristic_imp.py
@@ -4,8 +4,8 @@ from sample_generator import generate_random_graph_sample as generator
 
 def get_adj(edges, u):
     adj = []
-    for edge in edges and edge[1] not in adj:
-        if u == edge[0]:
+    for edge in edges:
+        if u == edge[0] and edge[1] not in adj:
             adj.append(edge[1])
         elif u == edge[1] and edge[0] not in adj:
             adj.append(edge[0])
@@ -14,18 +14,18 @@ def get_adj(edges, u):
 
 def vertex_cover_heuristic(edges, n_vertices):
 
-    # vertices = [vertex for vertex in range(1, n_vertices + 1)]
     visited = (n_vertices+1)*[False]
 
-    for u in range(1, n_vertices):
+    for u in range(1, n_vertices+1):
         if not visited[u]:
             for v in get_adj(edges, u):
                 if not visited[v]:
                     visited[u] = True
                     visited[v] = True
                     break
-    cover = [v for v in range(1, n_vertices) if visited[v] == True]
+    cover = [v for v in range(1, n_vertices+1) if visited[v] == True]
     return cover
 
 
+print(generator(5))
 print(vertex_cover_heuristic(generator(5), 5))
