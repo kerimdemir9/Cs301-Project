922b4ab8fdc1337255565d7f2b47848071f3aad7
Kerim Demir <kerim.demir2002@hotmail.com>
2023-05-27 13:23:55 +0300

    code changed

--- heuristic_imp.py
+++ heuristic_imp.py
@@ -4,45 +4,47 @@ import sys
 from brute_force_imp import vertex_cover_brute
 
 
-def get_adj(edges, u):
-    adj = []
+# def get_adj(edges, u):
+#     adj = []
+#     for edge in edges:
+#         if u == edge[0] and edge[1] not in adj:
+#             adj.append(edge[1])
+#         elif u == edge[1] and edge[0] not in adj:
+#             adj.append(edge[0])
+#     return adj
+
+
+# def vertex_cover_heuristic(edges, n_vertices, k):
+
+#     visited = (n_vertices+1)*[False]
+
+#     for u in range(1, n_vertices+1):
+#         if not visited[u]:
+#             for v in get_adj(edges, u):
+#                 if not visited[v]:
+#                     visited[u] = True
+#                     visited[v] = True
+#                     break
+#     cover = [v for v in range(1, n_vertices+1) if visited[v] == True]
+#     if len(cover) <= k:
+#         return [True, cover]
+#     else:
+#         return [False, cover]
+
+def remove(edges, u, v):
+    newEdges = []
     for edge in edges:
-        if u == edge[0] and edge[1] not in adj:
-            adj.append(edge[1])
-        elif u == edge[1] and edge[0] not in adj:
-            adj.append(edge[0])
-    return adj
+        if (u not in edge) and (v not in edge):
+            newEdges.append(edge)
+    return newEdges
 
 
-def vertex_cover_heuristic(edges, n_vertices, k):
-
-    visited = (n_vertices+1)*[False]
-
-    for u in range(1, n_vertices+1):
-        if not visited[u]:
-            for v in get_adj(edges, u):
-                if not visited[v]:
-                    visited[u] = True
-                    visited[v] = True
-                    break
-    cover = [v for v in range(1, n_vertices+1) if visited[v] == True]
-    if len(cover) <= k:
-        return [True, cover]
-    else:
-        return [False, cover]
-
-
-true = 0
-false = 0
-for i in range(20):
-    size = random.randint(1, 20)
-    min = random.randint(1, size)
-    graph = generator(size)
-    brute = vertex_cover_brute(graph, size, min)
-    heuristic = vertex_cover_heuristic(graph, size, min)
-    if heuristic[0] == brute[0]:
-        true += 1
-    else:
-        false += 1
-
-# print((true/20)*100)
+def vertex_cover_heuristic(edges):
+    vertexCover = []
+    UC = edges
+    while len(UC) > 0:
+        CE = UC[random.randint(0, len(UC)-1)]
+        vertexCover.append(CE[0])
+        vertexCover.append(CE[1])
+        UC = remove(UC, CE[0], CE[1])
+    return vertexCover
--- performance_test.py
+++ performance_test.py
@@ -7,38 +7,39 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def test(size, k):
+def test(size):
     start = time.time()
     graph = generator(size)
-    vertex_cover_heuristic(graph, size, k)
+    vertex_cover_heuristic(graph)
     end = time.time()
     return end-start
 
-# with confidence level 95% and N = 100000
 
-
-def exec(N, size, k):
+def exec(N, size):
     tests = []
     for i in range(N):
-        tests.append(test(size, k))
+        tests.append(test(size))
     # std = np.std(tests)
     mean = np.mean(tests)
     # sm = std/math.sqrt(N)
+    # t = 1.645
     return mean
-
 
 # graph for input sizes = [5,6,7,8,9,10]
 
 
+print(exec(100000, 10))
 inputSize = [5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
 results = []
 for i in range(5, 21):
-    results.append(exec(100000, i, random.randint(1, i)))
+    results.append(exec(100000, i))
 
 
 xlog = np.log(inputSize)
 ylog = np.log(results)
+slope, intercept = np.polyfit(xlog, ylog, 1)
 plt.loglog(inputSize, results)
+print(slope)
 # plt.scatter(inputSize, results)
 plt.xlabel("log(inputSize)")
 plt.ylabel("log(Running time mean)")
